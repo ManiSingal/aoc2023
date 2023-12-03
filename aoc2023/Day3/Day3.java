40367b7a5ab66a23469aeb95fee4a99a542f43cf
Mani Singal <mani88singal@gmail.com>
2023-12-03 21:26:48 +1100

    Finished Part 2 of Day 3

--- aoc2023/Day3/Day3.java
+++ aoc2023/Day3/Day3.java
@@ -3,21 +3,28 @@ package Day3;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class Day3 {
     private static final int SIZE = 140;
     private static final char[][] ENGINE_SCHEMATIC = new char[SIZE][SIZE];
     private static final String FILE_PATH = "/home/mani/AOC/aoc2023/Day3/input.txt";
 
+    private static final Map<Gear, List<Integer>> m_gearToAdjacentPartNumbers = new HashMap<>();
+
     public static void main(final String[] args) {
         try {
-            System.out.println("Sum of the part numbers in the engine schematic: " + calculateSumOfPartNumbers());
+            System.out.println("Sum of the gear ratios in the engine schematic: " + calculateSumOfGearRatios());
         } catch (final IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static long calculateSumOfPartNumbers() throws IOException {
+    private static long calculateSumOfGearRatios() throws IOException {
         loadSchematic();
 
         long totalSum = 0;
@@ -26,13 +33,17 @@ public class Day3 {
                 if (Character.isDigit(ENGINE_SCHEMATIC[row][col])) {
                     String partNumber = getPartNumber(row, col);
                     col += partNumber.length() - 1;
-
-                    if (isAdjacentToSymbol(row, col - partNumber.length() + 1, col)) {
-                        totalSum += Integer.parseInt(partNumber);
-                    }
+                    checkIfAdjacentToGear(row, col - partNumber.length() + 1, col, partNumber);
                 }
             }
         }
+
+        for (final List<Integer> partNumbers : m_gearToAdjacentPartNumbers.values()) {
+            if (partNumbers.size() == 2) {
+                totalSum += partNumbers.get(0) * partNumbers.get(1);
+            }
+        }
+
         return totalSum;
     }
 
@@ -55,36 +66,80 @@ public class Day3 {
         }
     }
 
-    private static boolean isAdjacentToSymbol(final int row, final int colStart, final int colEnd) {
+    private static void checkIfAdjacentToGear(final int row, final int colStart, final int colEnd,
+            final String partNumber) {
         final int start = Math.max(colStart - 1, 0);
         final int end = Math.min(colEnd + 1, SIZE - 1);
 
-        if (isAdjacentInRow(row - 1, start, end) || isAdjacentInRow(row + 1, start, end)) {
-            return true;
+        if (isAdjacentInRow(row - 1, start, end, partNumber) || isAdjacentInRow(row + 1, start, end, partNumber)) {
+            return;
         }
 
-        if (colStart > 0 && isSymbol(ENGINE_SCHEMATIC[row][colStart - 1])) {
-            return true;
+        if (colStart > 0 && isGear(row, colStart - 1, partNumber)) {
+            return;
         }
-        if (colEnd < SIZE - 1 && isSymbol(ENGINE_SCHEMATIC[row][colEnd + 1])) {
-            return true;
+        if (colEnd < SIZE - 1 && isGear(row, colEnd + 1, partNumber)) {
+            return;
         }
 
-        return false;
+        return;
     }
 
-    private static boolean isAdjacentInRow(final int row, final int startCol, final int endCol) {
+    private static boolean isAdjacentInRow(final int row, final int startCol, final int endCol,
+            final String partNumber) {
         if (row < 0 || row >= SIZE)
             return false;
         for (int col = startCol; col <= endCol; col++) {
-            if (isSymbol(ENGINE_SCHEMATIC[row][col])) {
+            if (isGear(row, col, partNumber)) {
                 return true;
             }
         }
         return false;
     }
 
-    private static boolean isSymbol(final char ch) {
-        return !Character.isDigit(ch) && ch != '.';
+    private static boolean isGear(final int row, final int col, final String partNumber) {
+        if (ENGINE_SCHEMATIC[row][col] == '*') {
+            final Gear gear = new Gear(row, col);
+            if (m_gearToAdjacentPartNumbers.containsKey(gear)) {
+                m_gearToAdjacentPartNumbers.get(gear).add(Integer.parseInt(partNumber));
+            } else {
+                final List<Integer> partNumbers = new ArrayList<>();
+                partNumbers.add(Integer.parseInt(partNumber));
+                m_gearToAdjacentPartNumbers.put(gear, partNumbers);
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private static class Gear {
+        private int m_row;
+        private int m_col;
+
+        public Gear(final int row, final int col) {
+            m_row = row;
+            m_col = col;
+        }
+
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) {
+                return true;
+            }
+
+            if (!(o instanceof Gear)) {
+                return false;
+            }
+
+            final Gear gear = (Gear) o;
+            return m_row == gear.m_row && m_col == gear.m_col;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(m_row, m_col);
+        }
     }
 }
